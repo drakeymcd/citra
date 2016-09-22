85a0ff5f24f35c67b14223cb5c9df7a479ec4200
DaMan <daman6009@comcast.net>
2016-09-22 10:27:40 -0400

    DAE QT5.5

--- src/citra_qt/configuration/configure_system.cpp
+++ src/citra_qt/configuration/configure_system.cpp
@@ -56,9 +56,9 @@ void ConfigureSystem::ReadSystemSettings() {
     // set username
     username = Service::CFG::GetUsername();
     // TODO(wwylele): Use this when we move to Qt 5.5
-    // ui->edit_username->setText(QString::fromStdU16String(username));
-    ui->edit_username->setText(
-        QString::fromUtf16(reinterpret_cast<const ushort*>(username.data())));
+    ui->edit_username->setText(QString::fromStdU16String(username));
+//  ui->edit_username->setText(
+//      QString::fromUtf16(reinterpret_cast<const ushort*>(username.data())));
 
     // set birthday
     std::tie(birthmonth, birthday) = Service::CFG::GetBirthday();
@@ -90,9 +90,9 @@ void ConfigureSystem::applyConfiguration() {
 
     // apply username
     // TODO(wwylele): Use this when we move to Qt 5.5
-    // std::u16string new_username = ui->edit_username->text().toStdU16String();
-    std::u16string new_username(
-        reinterpret_cast<const char16_t*>(ui->edit_username->text().utf16()));
+    std::u16string new_username = ui->edit_username->text().toStdU16String();
+//  std::u16string new_username(
+//      reinterpret_cast<const char16_t*>(ui->edit_username->text().utf16()));
     if (new_username != username) {
         Service::CFG::SetUsername(new_username);
         modified = true;
